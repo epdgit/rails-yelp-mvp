316df5469ea7287cfa5c5b0dae609624e3393029
Eduardo Dantas <eduardopimenteldantas@hotmail.com>
2020-09-17 17:37:24 -0300

    Restaurant controller ok

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,9 +14,16 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path(@restaurant.id)
   end
 
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
    
 
 end
